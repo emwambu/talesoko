644a0aeffc07827d5551f975a056d9b568a6a8f3
emwambu <91182581+emwambu@users.noreply.github.com>
2025-04-24 17:39:48 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
-// js/firebase.js
+// Import Firebase App module from CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyD_jj3fpq2VBpSVvTemxkxOjnLqct7Nd9c",
-  authDomain: "talesoko-f8a98.firebaseapp.com",
-  projectId: "talesoko-f8a98",
-  storageBucket: "talesoko-f8a98.firebasestorage.app",
-  messagingSenderId: "812197190940",
-  appId: "1:812197190940:web:492ccdc8811f256f442ed9",
-  measurementId: "G-LKHPDCV9HL"
+  apiKey: "AIzaSyD_jj3fpq2VBpSVvTemxkxOjnLqct7Nd9c",
+  authDomain: "talesoko-f8a98.firebaseapp.com",
+  projectId: "talesoko-f8a98",
+  storageBucket: "talesoko-f8a98.firebasestorage.app",
+  messagingSenderId: "812197190940",
+  appId: "1:812197190940:web:492ccdc8811f256f442ed9",
+  measurementId: "G-LKHPDCV9HL"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log("Firebase initialized for Talesoko");
